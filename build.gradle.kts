660003c896ba6ce35d289479d28c6bef6174a85f
Vlad Volkov <vlad@hiberbee.com>
2020-08-09 00:54:54 +0300

    Added default chart

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
   `java-library`
   idea
   id("com.github.ben-manes.versions") version "0.29.0"
-  id("com.google.cloud.tools.jib") version "2.4.0"
+  id("com.google.cloud.tools.jib") version "2.5.0"
+  id("com.google.cloud.tools.minikube") version "1.0.0-alpha.3"
   id("org.jetbrains.gradle.plugin.idea-ext") version "0.8.1"
   id("org.sonarqube") version "3.0"
   id("org.springframework.boot") version "2.3.2.RELEASE"
@@ -26,24 +27,34 @@ tasks.withType<Test> {
 }
 
 tasks.withType<Wrapper> {
-  gradleVersion = "6.6-rc-3"
+  gradleVersion = "6.6-rc-6"
   distributionType = Wrapper.DistributionType.ALL
 }
 
 configure<JibExtension> {
   from {
-    image = "openjdk:14"
+    image = "adoptopenjdk:14-openj9"
   }
   to {
-    image = "localhost:5000/hiberb/${project.name}"
+    image = "harbor.k8s.hiberbee.net/templates/java-spring"
     tags = setOf("latest")
   }
   container {
     jvmFlags = mutableListOf("-Xms512m")
-    mainClass = "dev.hiberbee.example.ExampleApplication"
+    mainClass = "dev.hiberbee.ExampleApplication"
     ports = listOf("8080")
     labels = mapOf("org.opencontainers.image.authors" to "Vlad Volkov <vlad@hiberbee.com>")
     format = ImageFormat.OCI
+  }
+}
+
+configure<org.sonarqube.gradle.SonarQubeExtension> {
+  properties {
+    property("sonar.login", System.getenv("SONAR_TOKEN"))
+    property("sonar.host.url", "https://sonarcloud.io")
+    property("sonar.project", project.name)
+    property("sonar.projectKey", project.name.toLowerCase())
+    property("sonar.organization", "hiberbee")
   }
 }
 
@@ -58,9 +69,12 @@ repositories {
 }
 
 dependencies {
+  implementation(platform("org.springframework.cloud:spring-cloud-dependencies:Hoxton.RELEASE"))
   implementation(platform("org.springframework.boot:spring-boot-dependencies:2.3.2.RELEASE"))
   implementation(platform("de.codecentric:spring-boot-admin-dependencies:2.2.4"))
-  annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:2.3.1.RELEASE"))
+  annotationProcessor(platform("org.springframework.boot:spring-boot-dependencies:2.3.2.RELEASE"))
+
+  implementation("org.springframework.cloud:spring-cloud-starter-kubernetes-all")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-cache")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
